5976f97bb41165d294e7da30e3518a9a419c77f4
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-28 15:58:12 +0700

    Praktikum Modul 4 Konsep Kelas, Akses Modifier, Method dan Method Parameter, Constructor
    Percobaan 11 : Method Tanpa Nilai Kembali Memiliki Parameter

---
+++ src/K_MethodC.java
@@ -0,0 +1,11 @@
+public class K_MethodC {
+
+    public static void main(String[] args) {
+        kurang(10,5);
+    }
+
+    private static void kurang (int param1, int param2) {
+        int hasil = param1 - param2;
+        System.out.println("" +param1+ "-" +param2+ "=" +hasil);
+    }
+}
