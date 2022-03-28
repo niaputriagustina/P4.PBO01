3ea912438f5b5c40cdd2d3370f2631ab3f3abdc2
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-28 15:50:45 +0700

    Praktikum Modul 4 Konsep Kelas, Akses Modifier, Method dan Method Parameter, Constructor
    Percobaan 8 : Metode Tanpa Nilai Kembali (Void)

---
+++ src/I_MethodA.java
@@ -0,0 +1,9 @@
+public class I_MethodA {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    private static void welcome () {
+        System.out.println("Selamat Belajar Java");
+    }
+}
