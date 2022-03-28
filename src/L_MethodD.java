113e5867339f796abab185727f268c23bcd57199
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-03-28 16:03:14 +0700

    Praktikum Modul 4 Konsep Kelas, Akses Modifier, Method dan Method Parameter, Constructor
    Percobaan 12 : Method Dengan Nilai Kembali Memiliki Parameter

---
+++ src/L_MethodD.java
@@ -0,0 +1,11 @@
+public class L_MethodD {
+
+    public static void main(String[] args) {
+        double hasilBagi = bagi(9,3);
+        System.out.println("Hasil Bagi = " +hasilBagi);
+    }
+
+    private static double bagi (double param1, double param2) {
+        return (param1 / param2);
+    }
+}
