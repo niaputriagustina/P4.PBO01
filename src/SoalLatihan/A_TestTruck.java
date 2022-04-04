6d49a59cadceadce11a8222a0e957ba7702cda09
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-04-04 09:25:01 +0700

    Praktikum Modul 4 Konsep Kelas, Akses Modifier, Method dan Method Parameter, Constructor
    Tugas Praktikum : 01. Mengimplementasikan UML Class Diagram Dalam Prpgram Untuk Class Truk

---
+++ src/SoalLatihan/A_TestTruck.java
@@ -0,0 +1,55 @@
+package SoalLatihan;
+
+class Truck {
+
+    //Membuat Variabel Global
+    double muatan;
+    double muatanMaks;
+
+    //Membuat Constructor Dengan Parameter
+    Truck (double muatanMaks) {
+        this.muatanMaks = muatanMaks;
+    }
+
+    //Membuat Mehtod
+    public double getMuatan () {
+        return muatan;
+    }
+
+    public void setMuatan (double muatan) {
+        this.muatan = muatan;
+    }
+
+    public double getMuatanMaks () {
+        return muatanMaks;
+    }
+
+    public void setMuatanMaks (double muatanMaks) {
+        this.muatanMaks = muatanMaks;
+    }
+
+    public void tambahMuatan (double berat) {
+        this.muatan = muatan+berat;
+    }
+}
+
+public class A_TestTruck {
+
+    public static void main(String[] args) {
+
+        //Membuat Objek
+        Truck truk = new Truck (1000);
+
+        //Mengakses Method
+        System.out.println("Muatan Maksimal = " + truk.getMuatanMaks());
+        truk.tambahMuatan(500.0);
+        System.out.println("Tambah Muatan : 500");
+        truk.tambahMuatan(350.0);
+        System.out.println("Tambah Muatan : 350");
+        truk.tambahMuatan(100.0);
+        System.out.println("Tambah Muatan : 100");
+        truk.tambahMuatan(150.0);
+        System.out.println("Tambah Muatan : 150");
+        System.out.println("Muatan Sekarang = " + truk.getMuatan());
+    }
+}
