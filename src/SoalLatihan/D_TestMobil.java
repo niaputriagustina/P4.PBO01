8f43e9f2e25c8ad11914509bd8d3549702f711c4
CheesyStars <sigitkurniawanmtsn@gmail.com>
2022-04-04 10:40:41 +0700

    Praktikum Modul 4 Konsep Kelas, Akses Modifier, Method dan Method Parameter, Constructor
    Tugas Praktikum : 03. Menganalisa, Membuat UML Class Diagram dan Implementasi Program

---
+++ src/SoalLatihan/D_TestMobil.java
@@ -0,0 +1,56 @@
+package SoalLatihan;
+
+//Membuat Class Mobil
+class Mobil {
+
+    //Membuat Variabel Global
+    String objek;
+    String merekMobil;
+    String warnaMobil;
+    String kategoriMobil;
+    int hargaSewa;
+    int jumlahUnit;
+
+    //Membuat Constructor Dengan Parameter
+    Mobil (String objek, String merekMobil, String warnaMobil, String kategoriMobil, int hargaSewa, int jumlahUnit) {
+        this.objek = objek;
+        this.merekMobil = merekMobil;
+        this.warnaMobil = warnaMobil;
+        this.kategoriMobil = kategoriMobil;
+        this.hargaSewa = hargaSewa;
+        this.jumlahUnit = jumlahUnit;
+    }
+
+    //Membuat Method
+    public void InfoMobil () {
+        System.out.print("" + objek);
+        System.out.print("\t" + merekMobil);
+        System.out.print("\t" + warnaMobil);
+        System.out.print("\t" + kategoriMobil);
+        System.out.print("\t" + hargaSewa);
+        System.out.print("\t" + jumlahUnit);
+    }
+}
+
+public class D_TestMobil {
+    public static void main(String[] args) {
+
+        //Membuat Objek
+        Mobil mobil1 = new Mobil ("mobil1", "Toyota"," Biru","     Minibus",2000, 7);
+        Mobil mobil2 = new Mobil ("mobil2", "Daihatsu", "Hitam", "Pick-up",   1500, 2);
+        Mobil mobil3 = new Mobil ("mobil3", "Suzuki", "Silver", " SUV",     1800, 5);
+        Mobil mobil4 = new Mobil ("mobil4", "Honda", " Merah", "   Sedan",  1300, 5);
+
+        //Menampilkan Isi Objek
+        System.out.println("Objek\t\tA\t\tB\t\tC\t\tD\t\tE");
+        mobil1.InfoMobil();
+        System.out.println();
+        mobil2.InfoMobil();
+        System.out.println();
+        mobil3.InfoMobil();
+        System.out.println();
+        mobil4.InfoMobil();
+
+
+    }
+}
